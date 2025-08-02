bf7b1a5f3c3769c94769538b47d78a231d6380a4
afzal <afzal@gmail.com>
2025-08-02 23:40:36 +0600

    file add

---
+++ Sieve_of_Eratosthenes.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isprime(int n){
+    return true ;
+}
+
+int main(){
+    int n;cin>>n;
+
+    if(isprime(n)){
+        cout<<"is prime"<<endl;
+    }
+    else{
+        cout<<"No prime"<<endl;
+    }
+    
+    return 0;
+}
